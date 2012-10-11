b6ddee3d66267d5a0038d3ae546c02b77fef6809
Dayme <daymegr@yahoo.es>
2012-10-11 17:48:04 +0100

    modificado

--- lib/circunferencia.rb
+++ lib/circunferencia.rb
@@ -1,10 +1,12 @@
 # File: circunferencia.rb 
+#!/usr/bin/env ruby
 
 class Circunferencia
 
  def initialize(perimetro)
     raise unless perimetro.is_a?(Numeric)
     raise ArgumentError, "no puede ser un numero negativo" unless perimetro >= 0
+    puts 'hola aqum'
     @p = perimetro
  end
 
